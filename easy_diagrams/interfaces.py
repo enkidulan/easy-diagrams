f109187014e4b15ccbe195d1463c5d1b2219cdcc
Maksym Shalenyi <maksym.shalenyi@gmail.com>
2025-02-15 15:05:17 -0800

    Declare IDiagramRepo methods

--- easy_diagrams/interfaces.py
+++ easy_diagrams/interfaces.py
@@ -1,4 +1,12 @@
+from typing import TYPE_CHECKING
+
 from zope.interface import Interface
+
+if TYPE_CHECKING:
+    from easy_diagrams.domain.diagram import Diagram
+    from easy_diagrams.domain.diagram import DiagramEdit
+    from easy_diagrams.domain.diagram import DiagramID
+    from easy_diagrams.domain.diagram import DiagramListItem
 
 
 class ISocialLoginProvider(Interface):
@@ -21,4 +29,23 @@ class IDiagramRenderer(Interface):
 class IDiagramRepo(Interface):
     """Interface for Diagram repository."""
 
-    # TODO: add methods
+    def __init__(user_id: str, dbsession: object, diagram_renderer: IDiagramRenderer):
+        """Initialize repository with user_id, dbsession and diagram_renderer."""
+
+    def create() -> "DiagramID":
+        """Create new diagram and return its ID."""
+
+    def get(diagram_id: "DiagramID") -> "Diagram":
+        """Get diagram by its ID."""
+
+    def delete(diagram_id: "DiagramID") -> None:
+        """Delete diagram by its ID."""
+
+    def list(offset: int, limit: int) -> list["DiagramListItem"]:
+        """Paginated diagrams list."""
+
+    def edit(diagram_id: "DiagramID", changes: "DiagramEdit") -> None:
+        """Edit diagram by its ID."""
+
+    def get_image_render(diagram_id: "DiagramID") -> bytes:
+        """Get diagram image render as bytes by its ID."""
